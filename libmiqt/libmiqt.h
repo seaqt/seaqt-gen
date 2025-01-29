9b875815eda8f3c4be2fa36f9109cbb504c81324
Jacek Sieka <arnetheduck@gmail.com>
2025-01-29 10:09:19 +0100

    Fix signal connection leak
    An additional `release` callback provides opportunity to clean up after
    a signal connection is terminated, using a functor whose destructor gets
    called by the qt infrastructure when the connection ends - other than
    that, the approach mimics the current lambad approach.

--- libmiqt/libmiqt.h
+++ libmiqt/libmiqt.h
@@ -2,26 +2,51 @@
 #ifndef MIQT_LIBMIQT_LIBMIQT_H
 #define MIQT_LIBMIQT_LIBMIQT_H
 
-#include <string.h>
+#include <stddef.h>
+#include <stdint.h>
 
 #ifdef __cplusplus
+
+namespace seeqt {
+struct release_callback {
+  void (*release)(intptr_t);
+
+  release_callback(void (*release)(intptr_t)) : release(release) {}
+  release_callback(release_callback &&rhs) : release(rhs.release) {
+    rhs.release = 0;
+  }
+  release_callback &operator=(release_callback &&rhs) {
+    release = rhs.release;
+    rhs.release = 0;
+    return *this;
+  };
+
+  void operator()(intptr_t slot) {
+    if (release)
+      release(slot);
+  }
+  release_callback(const release_callback &) = delete;
+  release_callback &operator=(const release_callback &) = delete;
+};
+} // namespace seeqt
+
 extern "C" {
 #endif
 
 struct miqt_string {
-    size_t len;
-    char* data;
+  size_t len;
+  char *data;
 };
 
 struct miqt_array {
-    size_t len;
-    void* data;
+  size_t len;
+  void *data;
 };
 
 struct miqt_map {
-    size_t len;
-    void* keys;
-    void* values;
+  size_t len;
+  void *keys;
+  void *values;
 };
 
 typedef const char const_char;
