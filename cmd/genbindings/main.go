arnetheduck/nim-miqt-poc

Proof-of-concept nim bindings
This is a trivial POC that reuses the generated C from miqt to create
low-level Nim bindings, enough to run a simple Hello world test.

The high-level bindings in this POC are obviously not ready to be used
(ie there are memory leaks etc) and would need more work (ref vs value
types, inheritance structurec, how slots are represented and connected,
etc), probably to bring them closer to nimqt/nimqml/DOtherSide _or_
PySide.

Rough overview of changes needed to the go-based bindings:

* naming fixes - nim and go have different naming conventions - in the
bindings, some of this code currently resides in the C/IL part and
should probably be moved elsewhere
* packaging differences - nim files are imported much like header files
in C meaning it's reasonable to establish a 1:1 relationship between Qt
header and Nim file - this gives a nice "just-works" build experience
where the generated `c++` file is compiled together with the nim project
* slightly more canonical C types used - ie `void*` where a pointer is
intended instead of `intptr_t` which introduces unnecessary conversion
* avoid `_cgo_export.h` and instead generate the expected callback
prototypes in the C generator - currently, they get added to the
generated header file which is slightly wrong - they should reside in
the cpp file really.
* types and inheritance structure lives in a separate module to avoid
circular imports (which nim has only limited support for)

cmd/genbindings/main.go
@@ -91,8 +91,11 @@ func pkgConfigCflags(packageName string) string {
 
 	return string(stdout)
 }
+func moduleName(header string) string {
+	return "gen_" + strings.TrimSuffix(filepath.Base(header), `.h`)
+}
 
-func generate(packageName string, srcDirs []string, allowHeaderFn func(string) bool, clangBin, cflagsCombined, outDir string, matcher ClangMatcher) {
+func generate(packageName string, srcDirs []string, allowHeaderFn func(string) bool, clangBin, cflagsCombined, pkgConfigModule, outDir string, matcher ClangMatcher) {
 
 	var includeFiles []string
 	for _, srcDir := range srcDirs {
@@ -163,6 +166,21 @@ func generate(packageName string, srcDirs []string, allowHeaderFn func(string) b
 		astTransformConstructorOrder(parsed)
 		atr.Process(parsed)
 
+		// Hack to add some overloads
+		if strings.Contains(inputHeader, "qvariant.h") {
+			for i, c := range parsed.Classes {
+				if c.ClassName == "QVariant" {
+					m := CppMethod{
+						MethodName: "fromValue",
+						ReturnType: CppParameter{ParameterType: "QVariant"},
+						Parameters: []CppParameter{CppParameter{ParameterName: "value", ParameterType: "QObject", Const: true, Pointer: true}},
+						IsStatic:   true,
+					}
+					parsed.Classes[i].Methods = append(parsed.Classes[i].Methods, m)
+				}
+			}
+		}
+
 		// Update global state tracker (AFTER astTransformChildClasses)
 		addKnownTypes(packageName, parsed)
 
@@ -172,6 +190,12 @@ func generate(packageName string, srcDirs []string, allowHeaderFn func(string) b
 	//
 	// PASS 2
 	//
+
+	libsSrc := fmt.Sprintf(`
+const libs = gorge("pkg-config -libs %s")
+{.passl: libs}
+`, pkgConfigModule)
+	os.WriteFile(filepath.Join(outDir, strings.ReplaceAll(pkgConfigModule, " ", "_")+"_libs.nim"), []byte(libsSrc), 0644)
 
 	for _, parsed := range processHeaders {
 
@@ -201,7 +225,7 @@ func generate(packageName string, srcDirs []string, allowHeaderFn func(string) b
 		}
 
 		// Emit 3 code files from the intermediate format
-		outputName := filepath.Join(outDir, "gen_"+strings.TrimSuffix(filepath.Base(parsed.Filename), `.h`))
+		outputName := filepath.Join(outDir, moduleName(parsed.Filename))
 
 		// For packages where we scan multiple directories, it's possible that
 		// there are filename collisions (e.g. Qt 6 has QtWidgets/qaction.h include
@@ -224,7 +248,10 @@ func generate(packageName string, srcDirs []string, allowHeaderFn func(string) b
 
 		goSrc, go64Src, err := emitGo(parsed, filepath.Base(parsed.Filename), packageName)
 		if err != nil {
-			panic(err)
+			log.Printf("Error in %s: %s", parsed.Filename, err)
+			continue
+
+			// panic(err)
 		}
 
 		err = os.WriteFile(outputName+".go", []byte(goSrc), 0644)
@@ -234,6 +261,26 @@ func generate(packageName string, srcDirs []string, allowHeaderFn func(string) b
 
 		if len(go64Src) > 0 {
 			err = os.WriteFile(outputName+"_64bit.go", []byte(go64Src), 0644)
+			if err != nil {
+				panic(err)
+			}
+		}
+
+		nimSrc, nim64Src, err := emitNim(parsed, filepath.Base(parsed.Filename), packageName, pkgConfigModule)
+		if err != nil {
+			log.Printf("Error in %s: %s", parsed.Filename, err)
+			continue
+
+			// panic(err)
+		}
+
+		err = os.WriteFile(outputName+".nim", []byte(nimSrc), 0644)
+		if err != nil {
+			panic(err)
+		}
+
+		if len(nim64Src) > 0 {
+			err = os.WriteFile(outputName+"_types.nim", []byte(nim64Src), 0644)
 			if err != nil {
 				panic(err)
 			}

cmd/genbindings/statetracker.go
@@ -2,16 +2,19 @@ package main
 
 type lookupResultClass struct {
 	PackageName string
+	ModuleName  string
 	Class       CppClass
 }
 
 type lookupResultTypedef struct {
 	PackageName string
+	ModuleName  string
 	Typedef     CppTypedef
 }
 
 type lookupResultEnum struct {
 	PackageName string
+	ModuleName  string
 	Enum        CppEnum
 }
 
@@ -29,12 +32,12 @@ func flushKnownTypes() {
 
 func addKnownTypes(packageName string, parsed *CppParsedHeader) {
 	for _, c := range parsed.Classes {
-		KnownClassnames[c.ClassName] = lookupResultClass{packageName, c /* copy */}
+		KnownClassnames[c.ClassName] = lookupResultClass{packageName, moduleName(parsed.Filename), c /* copy */}
 	}
 	for _, td := range parsed.Typedefs {
-		KnownTypedefs[td.Alias] = lookupResultTypedef{packageName, td /* copy */}
+		KnownTypedefs[td.Alias] = lookupResultTypedef{packageName, moduleName(parsed.Filename), td /* copy */}
 	}
 	for _, en := range parsed.Enums {
-		KnownEnums[en.EnumName] = lookupResultEnum{packageName, en /* copy */}
+		KnownEnums[en.EnumName] = lookupResultEnum{packageName, moduleName(parsed.Filename), en /* copy */}
 	}
 }

seaqt/seaqt-gen

cmd/genbindings/main.go
@@ -60,6 +60,9 @@ func pkgConfigCflags(pcName string) string {
 
 	return string(stdout)
 }
+func genUnitName(header string) string {
+	return "gen_" + strings.TrimSuffix(filepath.Base(header), `.h`)
+}
 
 func generate(moduleName, pcName string, srcDirs []string, clangBin, cflagsExtras, outDir string) {
 
@@ -75,6 +78,7 @@ func generate(moduleName, pcName string, srcDirs []string, clangBin, cflagsExtra
 	log.Printf("Found %d header files to process.", len(includeFiles))
 
 	cflags := append(strings.Fields(cflagsExtras), strings.Fields(pkgConfigCflags(pcName))...)
+	baseDir := outDir
 	outDir = filepath.Join(outDir, moduleName)
 	os.MkdirAll(outDir, 0755)
 
@@ -130,6 +134,21 @@ func generate(moduleName, pcName string, srcDirs []string, clangBin, cflagsExtra
 		astTransformConstructorOrder(parsed)
 		atr.Process(parsed)
 
+		// Hack to add some overloads
+		if strings.Contains(inputHeader, "qvariant.h") {
+			for i, c := range parsed.Classes {
+				if c.ClassName == "QVariant" {
+					m := CppMethod{
+						MethodName: "fromValue",
+						ReturnType: CppParameter{ParameterType: "QVariant"},
+						Parameters: []CppParameter{CppParameter{ParameterName: "value", ParameterType: "QObject", Const: true, Pointer: true}},
+						IsStatic:   true,
+					}
+					parsed.Classes[i].Methods = append(parsed.Classes[i].Methods, m)
+				}
+			}
+		}
+
 		// Update global state tracker (AFTER astTransformChildClasses)
 		addKnownTypes(moduleName, parsed)
 
@@ -139,6 +158,12 @@ func generate(moduleName, pcName string, srcDirs []string, clangBin, cflagsExtra
 	//
 	// PASS 2
 	//
+
+	libsSrc := fmt.Sprintf(`
+const libs = gorge("pkg-config --libs %s")
+{.passl: libs}
+`, pcName)
+	os.WriteFile(filepath.Join(outDir, strings.ReplaceAll(pcName, " ", "_")+"_libs.nim"), []byte(libsSrc), 0644)
 
 	for _, parsed := range processHeaders {
 
@@ -168,7 +193,7 @@ func generate(moduleName, pcName string, srcDirs []string, clangBin, cflagsExtra
 		}
 
 		// Emit 3 code files from the intermediate format
-		outputName := filepath.Join(outDir, "gen_"+strings.TrimSuffix(filepath.Base(parsed.Filename), `.h`))
+		outputName := filepath.Join(outDir, genUnitName(parsed.Filename))
 
 		// For packages where we scan multiple directories, it's possible that
 		// there are filename collisions (e.g. Qt 6 has QtWidgets/qaction.h include
@@ -189,6 +214,36 @@ func generate(moduleName, pcName string, srcDirs []string, clangBin, cflagsExtra
 			counter++
 		}
 
+		nimSrc, nim64Src, err := emitNim(parsed, filepath.Base(parsed.Filename), moduleName, pcName)
+		if err != nil {
+			log.Printf("Error in %s: %s", parsed.Filename, err)
+			continue
+
+			// panic(err)
+		}
+
+		err = os.WriteFile(outputName+".nim", []byte(nimSrc), 0644)
+		if err != nil {
+			panic(err)
+		}
+
+		if len(nim64Src) > 0 {
+			err = os.WriteFile(outputName+"_types.nim", []byte(nim64Src), 0644)
+			if err != nil {
+				panic(err)
+			}
+		}
+
+		for _, c := range parsed.Classes {
+			if strings.Contains(c.ClassName, "::") {
+				// Inner classes are covered by their main declaration
+				continue
+			}
+
+			os.WriteFile(
+				filepath.Join(baseDir, strings.ToLower(c.ClassName)+".nim"),
+				[]byte("import ./"+moduleName+"/"+genUnitName(parsed.Filename)+"\nexport "+genUnitName(parsed.Filename)), 0644)
+		}
 		bindingCppSrc, err := emitBindingCpp(parsed, filepath.Base(parsed.Filename))
 		if err != nil {
 			panic(err)

cmd/genbindings/statetracker.go
@@ -2,16 +2,19 @@ package main
 
 type lookupResultClass struct {
 	PackageName string
+	UnitName    string
 	Class       CppClass
 }
 
 type lookupResultTypedef struct {
 	PackageName string
+	UnitName    string
 	Typedef     CppTypedef
 }
 
 type lookupResultEnum struct {
 	PackageName string
+	UnitName    string
 	Enum        CppEnum
 }
 
@@ -29,12 +32,12 @@ func flushKnownTypes() {
 
 func addKnownTypes(packageName string, parsed *CppParsedHeader) {
 	for _, c := range parsed.Classes {
-		KnownClassnames[c.ClassName] = lookupResultClass{packageName, c /* copy */}
+		KnownClassnames[c.ClassName] = lookupResultClass{packageName, genUnitName(parsed.Filename), c /* copy */}
 	}
 	for _, td := range parsed.Typedefs {
-		KnownTypedefs[td.Alias] = lookupResultTypedef{packageName, td /* copy */}
+		KnownTypedefs[td.Alias] = lookupResultTypedef{packageName, genUnitName(parsed.Filename), td /* copy */}
 	}
 	for _, en := range parsed.Enums {
-		KnownEnums[en.EnumName] = lookupResultEnum{packageName, en /* copy */}
+		KnownEnums[en.EnumName] = lookupResultEnum{packageName, genUnitName(parsed.Filename), en /* copy */}
 	}
 }